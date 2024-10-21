b947f4ef4cc5711cb70c547319dd8b1cb49549d5
Sharjeel Akhtar <sharjeelakhtar245@gmail.com>
2024-10-21 23:43:26 +0500

    collection

--- src/Components/Home/Arrivals.jsx
+++ src/Components/Home/Arrivals.jsx
@@ -59,7 +59,7 @@ const Arrivals = () => {
                     <p>Check your internet connection and try again!</p>
                 ) : (
                     products?.map((item, index) => (
-                        <div className="col card" key={index} data-aos='fade-up'>
+                        <div className="col card border-0 border-bottom border-light shadow-sm" key={index} data-aos='fade-up'>
                             <a href={`/product/${item.title.replace(/ /g, '-')}/${item._id}`}>
                                 <div className="card_img">
                                     <img src={item?.images[0]} className="text-center" alt={item?.title} />
